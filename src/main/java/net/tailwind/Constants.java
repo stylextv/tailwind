b4b0131c8aec2af430a675122078abae630f580b
StylexTV <56502218+StylexTV@users.noreply.github.com>
2022-06-09 18:47:54 +0200

    Create Constants.java

---
+++ src/main/java/net/tailwind/Constants.java
@@ -0,0 +1,9 @@
+package net.tailwind;
+
+public class Constants {
+	
+	public static final String NAME = "Tailwind";
+	public static final String VERSION = "v0.1.0+build.10";
+	public static final String AUTHOR = "StylexTV";
+	
+}

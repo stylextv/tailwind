a42c095f10f68dbd351714e7a54ad3bd7126e7e9
StylexTV <56502218+StylexTV@users.noreply.github.com>
2022-06-16 15:45:47 +0200

    Added axis goal

---
+++ src/main/java/net/tailwind/path/search/goal/goals/AxisGoal.java
@@ -0,0 +1,40 @@
+package net.tailwind.path.search.goal.goals;
+
+import net.minecraft.util.math.BlockPos;
+import net.tailwind.path.search.goal.Goal;
+
+public class AxisGoal extends Goal {
+	
+	@Override
+	public double cost(BlockPos pos) {
+		return absoluteCost(pos);
+	}
+	
+	@Override
+	public boolean isTerminal(BlockPos pos) {
+		int x = pos.getX();
+		int z = pos.getZ();
+		
+		return x == 0 || z == 0 || Math.abs(x) == Math.abs(z);
+	}
+	
+	@Override
+	public String toString() {
+		return "AxisGoal";
+	}
+	
+	public static double absoluteCost(BlockPos pos) {
+		int x = pos.getX();
+		int z = pos.getZ();
+		
+		return absoluteCost(x, z);
+	}
+	
+	public static double absoluteCost(int x, int z) {
+		x = Math.abs(x);
+		z = Math.abs(z);
+		
+		
+	}
+	
+}

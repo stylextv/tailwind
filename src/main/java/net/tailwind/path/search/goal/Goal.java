690b5032f503b0638c0d8326025805b8cdb4797f
StylexTV <56502218+StylexTV@users.noreply.github.com>
2022-06-19 17:46:05 +0200

    Update Goal.java

--- src/main/java/net/tailwind/path/search/goal/Goal.java
+++ src/main/java/net/tailwind/path/search/goal/Goal.java
@@ -1,9 +1,14 @@
 package net.tailwind.path.search.goal;
 
 import net.minecraft.util.math.BlockPos;
+import net.tailwind.path.search.goal.goals.AxisGoal;
+import net.tailwind.path.search.goal.goals.BlockGoal;
 import net.tailwind.path.search.node.Node;
 
 public abstract class Goal {
+	
+	public static final Goal ORIGIN = new BlockGoal(BlockPos.ORIGIN);
+	public static final Goal AXIS = new AxisGoal();
 	
 	public abstract double cost(BlockPos pos);
 	public abstract boolean isTerminal(BlockPos pos);

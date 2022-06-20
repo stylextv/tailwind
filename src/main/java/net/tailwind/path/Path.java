8e50c30d93bfb2b0ab0c5c701fdd8a44ddcb7238
StylexTV <56502218+StylexTV@users.noreply.github.com>
2022-06-20 12:39:01 +0200

    Update Path.java

--- src/main/java/net/tailwind/path/Path.java
+++ src/main/java/net/tailwind/path/Path.java
@@ -1,7 +1,22 @@
 package net.tailwind.path;
 
+import net.tailwind.path.search.node.Node;
+
 public class Path {
 	
 	
+	
+	public static Path fromNode(Node node) {
+		Path path = new Path();
+		
+		while(node != null) {
+			
+			// add node to path
+			
+			node = node.getParent();
+		}
+		
+		return path;
+	}
 	
 }
